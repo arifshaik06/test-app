0ac18d3273f35dc66daebf1c777cfd8681686d57
nikhilchand61 <nikhilchand61@gmail.com>
2024-09-07 18:13:36 +0530

    Test app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-app';
+  showPopup = false;
+  offcanvasVisible = true;
+  progress = 0;
+  intervalId: any;
+
+  // Start the migration process
+  startProcess() {
+    this.showPopup = true;
+    this.progress = 0;
+    this.offcanvasVisible = true;
+
+    this.intervalId = setInterval(() => {
+      if (this.progress < 100) {
+        this.progress += 5; // Update progress
+      } else {
+        clearInterval(this.intervalId);
+        this.showPopup = false;
+        alert('Migration process has been completed');
+      }
+    }, 500); // Adjust the speed as needed
+  }
+
+  // Toggle the visibility of the offcanvas
+  togglePopup() {
+    this.offcanvasVisible = !this.offcanvasVisible;
+  }
 }
